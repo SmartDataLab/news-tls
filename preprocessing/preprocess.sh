bc4606d454385754a48c48e9f877ef1e02c151f6
JinhuaSu <944866518@qq.com>
2020-11-19 08:38:00 +0000

    生成study case的过程中修改脚本文件

--- preprocessing/preprocess.sh
+++ preprocessing/preprocess.sh
@@ -1,6 +1,6 @@
 #!bash
-DATASET=../../../data/datasets/test
-HEIDELTIME=../../../tilse-master/tilse/tools/heideltime
+DATASET=/data1/su/app/text_forecast/hxw/data/test
+HEIDELTIME=/data1/su/app/text_forecast/tilse-master/tilse/tools/heideltime
 python preprocess_tokenize.py --dataset $DATASET
 python preprocess_heideltime.py --dataset $DATASET --heideltime $HEIDELTIME
 python preprocess_spacy.py --dataset $DATASET
--- run_cluster.sh
+++ run_cluster.sh
@@ -1,4 +1,4 @@
 #!bash
-DATASET=../../data/datasets/test
-RESULTS=../../results
-python experiments/run_without_eval.py --dataset $DATASET --method clust --output $RESULTS/test/clust.json
+DATASET=/data1/su/app/text_forecast/hxw/data/test
+RESULTS=/data1/su/app/text_forecast/hxw/data/sota_res
+python experiments/run_without_eval.py --dataset $DATASET --method clust --output $RESULTS/clust.json
